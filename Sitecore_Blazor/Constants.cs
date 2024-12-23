1a7e010a9530cd4e129a6f0425f13ade51a1d297
Jeel <vachhanijeel50@gmail.com>
2024-12-23 11:36:21 +0530

    change in jss app node

--- Sitecore_Blazor/Constants.cs
+++ Sitecore_Blazor/Constants.cs
@@ -5,7 +5,7 @@
         public const string SitecoreCMUrl = "https://scblazor104sc.dev.local";
         public const string LayoutRender = "/sitecore/api/layout/render/jss";
         public const string ItemApi = "?item=";
-        public const string ProjectRoot = "/sitecore/content/sitecore-blazor/";
+        public const string ProjectRoot = "/sitecore/content/sitecore-jss-app/";
         public const string SCApiKey = "{0A5D6ACB-F648-41AF-8747-2614D5B041FB}";
 
         public const string GetContentByUrlEndpoint = "/api/ContentApi/GetByUrl?path=";
