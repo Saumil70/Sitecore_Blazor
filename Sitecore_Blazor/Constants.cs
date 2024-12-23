d13194618462494d67870f91ded32b93e9ea38ad
Jeel <vachhanijeel50@gmail.com>
2024-12-23 14:39:15 +0530

    Dictionary added

--- Sitecore_Blazor/Constants.cs
+++ Sitecore_Blazor/Constants.cs
@@ -4,6 +4,7 @@
     {
         public const string SitecoreCMUrl = "https://scblazor104sc.dev.local";
         public const string LayoutRender = "/sitecore/api/layout/render/jss";
+        public const string DictionaryRender = "/sitecore/api/jss/dictionary/";
         public const string ItemApi = "?item=";
         public const string ProjectRoot = "/sitecore/content/sitecore-jss-app/";
         public const string SCApiKey = "{0A5D6ACB-F648-41AF-8747-2614D5B041FB}";
@@ -12,5 +13,6 @@
         public const string GetHeaderEndpoint = "/api/HeaderViewComponent/GetHeaderData";
         public const string GetFooterEndpoint = "/api/FooterViewComponent/GetFooterData";
         public const string UrlWithVersion = "scblazor104sc.dev.local/en/";
+        public const string AppNameWithVersion = "sitecore-blazor/en";
     }
 }
