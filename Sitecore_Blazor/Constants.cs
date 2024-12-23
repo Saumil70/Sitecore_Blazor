7951189b73c8baff624020ec6675266565b6504f
Jeel <vachhanijeel50@gmail.com>
2024-12-23 11:16:18 +0530

    initial

--- Sitecore_Blazor/Constants.cs
+++ Sitecore_Blazor/Constants.cs
@@ -2,11 +2,11 @@
 {
     public static class Constants
     {
-        public const string SitecoreCMUrl = "https://sc104blazorsc.dev.local";
+        public const string SitecoreCMUrl = "https://scblazor104sc.dev.local";
         public const string LayoutRender = "/sitecore/api/layout/render/jss";
         public const string ItemApi = "?item=";
-        public const string ProjectRoot = "/sitecore/content/sitecore-jss-app/";
-        public const string SCApiKey = "{C2B5071F-9C42-43FA-B713-D4C6BC520955}";
+        public const string ProjectRoot = "/sitecore/content/sitecore-blazor/";
+        public const string SCApiKey = "{0A5D6ACB-F648-41AF-8747-2614D5B041FB}";
 
         public const string GetContentByUrlEndpoint = "/api/ContentApi/GetByUrl?path=";
         public const string GetHeaderEndpoint = "/api/HeaderViewComponent/GetHeaderData";
